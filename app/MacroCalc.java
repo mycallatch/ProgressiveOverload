46bc31f10424b1250e2aeec11b0b9ea125658d16
Mykeroni <mycal.latchman@outlook.com>
2023-09-09 17:43:29 +0100

    Created a method for finding male Base Metabolic Rate.
    Must create an option to be female.

--- app/MacroCalc.java
+++ app/MacroCalc.java
@@ -2,13 +2,27 @@ package app;
 
 public class MacroCalc {
     float weight;
+    float calories;
+    float height;
+    float age;
     private double proteinIntake;
+    private double caloricIntake;
 
     public MacroCalc() {
         weight = 0;
+        height = 0;
+        age = 0;
+        calories = 0;
         proteinIntake = 0;
+        caloricIntake = 0;
     }
      
+    public double caloriesIn(float calories) {
+        MacroCalc body = new MacroCalc();
+        body.caloricIntake =  88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+        return body.caloricIntake;
+    }
+
     public double proteinIn(float weight) {
         MacroCalc body = new MacroCalc();
         body.proteinIntake = weight * 1.2;
