c346cfbbab7e4533f7495b1b4920dc030b2f62a7
Mykeroni <mycal.latchman@outlook.com>
2023-09-09 13:53:57 +0100

    Created proteinIn().

---
+++ java/MacroCalc.java
@@ -0,0 +1,16 @@
+package java;
+
+public class MacroCalc {
+    double weight;
+    private double proteinIntake;
+
+    public MacroCalc() {
+        weight = 0.0;
+    }
+     
+    public double proteinIn(double weight) {
+        MacroCalc body = new MacroCalc();
+        proteinIntake = body.weight * 1.2;
+        return proteinIntake;
+    }
+}
