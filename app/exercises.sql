a81ab1aabd100596ba89172483dbc569ee1056c7
Mykeroni <mycal.latchman@outlook.com>
2023-09-14 17:16:29 +0100

    Created inserts for tricep workouts

--- app/exercises.sql
+++ app/exercises.sql
@@ -13,4 +13,12 @@ INSERT INTO exercise VALUES(1, 'Dumbell Curls', 'biceps', 'pull', 'arms', 'Gym')
 INSERT INTO exercise VALUES(2, 'Preacher Curls', 'biceps', 'pull', 'arms', 'Gym');
 INSERT INTO exercise VALUES(3, 'Cable Curls', 'biceps', 'pull', 'arms', 'Gym');
 INSERT INTO exercise VALUES(4, 'Hammer Curls', 'biceps', 'pull', 'arms', 'Gym');
-INSERT INTO exercise VALUES(4, 'Barbell Curls', 'biceps', 'pull', 'arms', 'Gym');
+INSERT INTO exercise VALUES(5, 'Barbell Curls', 'biceps', 'pull', 'arms', 'Gym');
+
+-- Inserts for tricep workouts
+INSERT INTO exercise VALUES(6, 'Tricep Extension', 'triceps', 'push', 'arms', 'Gym');
+INSERT INTO exercise VALUES(7, 'Tricep Push-Down', 'triceps', 'push', 'arms', 'Gym');
+INSERT INTO exercise VALUES(8, 'Skull Crushers', 'triceps', 'push', 'arms', 'Gym');
+INSERT INTO exercise VALUES(9, 'Tricep Dips', 'triceps', 'push', 'arms', 'Home');
+INSERT INTO exercise VALUES(10, 'Diamond Push-Ups', 'triceps', 'push', 'arms', 'Home');
+INSERT INTO exercise VALUES(11, 'Tricep Dumbell Kickbacks', 'triceps', 'push', 'arms', 'Gym');
