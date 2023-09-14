69b7b7a244a8097a6a7afde3acf0fc091b713633
Mykeroni <mycal.latchman@outlook.com>
2023-09-14 17:06:01 +0100

    Created a table for exercises.

---
+++ app/exercises.sql
@@ -0,0 +1,11 @@
+CREATE TABLE exercise (
+	exercise_id INT,
+	exercise_name VARCHAR(20),
+	exercise_target VARCHAR(20),
+    exercise_goal VARCHAR(20),
+    exercise_group VARCHAR(20),
+	exercise_location VARCHAR(20),
+	PRIMARY KEY (exercise_id)
+);
+
+
