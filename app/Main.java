82188dc1240424eb1a63323ff462021f0e181c6d
Mykeroni <mycal.latchman@outlook.com>
2023-09-18 14:25:28 +0100

    Created basic webpage for home screen.

--- app/Main.java
+++ app/Main.java
@@ -12,32 +12,35 @@ public class Main {
     private static float ageInt;
 
     public static void main(String[] args) {
-        Scanner user = new Scanner(System.in);
-        System.out.println("How heavy are you in kg? ");
-        String weight = user.nextLine();
-        weightInt = Integer.parseInt(weight);
+        try (Scanner user = new Scanner(System.in)) {
+            System.out.println("How heavy are you in kg? ");
+            String weight = user.nextLine();
+            weightInt = Integer.parseInt(weight);
 
-        System.out.println("How tall are you in cm? ");
-        String height = user.nextLine();
-        heightInt = Integer.parseInt(height);
+            System.out.println("How tall are you in cm? ");
+            String height = user.nextLine();
+            heightInt = Integer.parseInt(height);
 
-        System.out.println("How old are you in years? ");
-        String age = user.nextLine();
-        ageInt = Integer.parseInt(age);
+            System.out.println("How old are you in years? ");
+            String age = user.nextLine();
+            ageInt = Integer.parseInt(age);
 
-        System.out.println("Were you born male or female? (m/f) ");
-        String sex = user.nextLine();
+            System.out.println("Were you born male or female? (m/f) ");
+            String sex = user.nextLine();
 
-        System.out.println("Are you aiming to recompose, bulk or cut? (r/b/c) ");
-        String goal = user.nextLine();
+            System.out.println("Are you aiming to recompose, bulk or cut? (r/b/c) ");
+            String goal = user.nextLine();
 
-        System.out.println("How active are you? (1-5) ");
-        String activityLevel = user.nextLine();
+            System.out.println("How active are you? (1-5) ");
+            String activityLevel = user.nextLine();
 
-        MacroCalc protein = new MacroCalc();
-        proteinIntake = protein.proteinIn(weightInt, goal);
-        MacroCalc calories = new MacroCalc();
-        caloricIntake = calories.caloriesIn(weightInt, heightInt, ageInt, sex, activityLevel, goal);
+            MacroCalc protein = new MacroCalc();
+            proteinIntake = protein.proteinIn(weightInt, goal);
+            MacroCalc calories = new MacroCalc();
+            caloricIntake = calories.caloriesIn(weightInt, heightInt, ageInt, sex, activityLevel, goal);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
         MacroCalc carbohydrates = new MacroCalc();
         carbIntake = carbohydrates.carbsIn(caloricIntake);
         MacroCalc fats = new MacroCalc();
